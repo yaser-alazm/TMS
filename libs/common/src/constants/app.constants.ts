f4d4518b6dab73b5876646701b448da4f6743e6e
yaser-alazm <yaser.azem@gmail.com>
2025-08-06 11:34:50 +0200

    extract common lib into seperated lib

--- libs/common/src/constants/app.constants.ts
+++
@@ -1,1 +0,0 @@
-export const APP_NAME = 'TMS - Trans Management System'
--- libs/common/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './dtos/example.dto'
-export * from './constants/app.constants'
-export * from './interfaces/user.interface'
